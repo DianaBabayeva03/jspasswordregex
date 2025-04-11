8444bee38a2b8452e168d41241b52162eb40f6f5
Fidan Mammadova <mammadova.fidan@sdu.edu.az>
2025-04-11 10:38:54 +0400

    password regex

---
+++ main.js
@@ -0,0 +1,23 @@
+// Sual 1. Daxil edeceyiniz adin bash herfini boyuk, qalan 
+// herflerini kicik herflerle qaytaran bir f-ya yazin.
+
+// function daxilEdeceyimizAd(ad) {
+//     return ad.charAt(0).toUpperCase() + 
+//     ad.slice(1).toLowerCase()
+// }
+// console.log(daxilEdeceyimizAd("samire"));
+
+
+
+// Sual 2. Istifadecinin daxil etdiyi shifrenin guclu
+// olub olmadigini yoxlayan bir f-ya yazin. Shifrenin guclu
+// olmasi uchun en az - bir boyuk herf, bir reqem ve xususi
+// bir simvol olmalidir. F-ya true ve ya false qaytarmalidir.
+// js password regex
+
+// function gucluparol(password) {
+//     return/[A-Z]/.test(password) && /\d/.test(password) && 
+//     /[!@$%^&*()_+12345678...]/.test(password)
+// }
+// console.log(gucluparol("password123!"));
+// console.log(gucluparol("Password1234"));
